c3b084f7743baa0f8e6167036c6e17c01e4b0068
AR413 <s193254@student.dtu.dk>
2021-04-22 19:58:54 +0000

    update load to write out data as tsv

--- R/01_load.R
+++ R/01_load.R
@@ -4,12 +4,13 @@ rm(list = ls())
 
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
+library("readxl")
 
 
 # Load data ---------------------------------------------------------------
-my_data_raw <- read_tsv(file = "data/data_raw.s001.csv")
+my_data_raw <- read_xls("data/_raw/pone.0207943.s001.xls")
 #my_data_raw
 
-# Write data --------------------------------------------------------------
-write_tsv(x = my_data,
+# Write data
+write_tsv(x = my_data_raw,
           file = "data/01_my_data.tsv")
