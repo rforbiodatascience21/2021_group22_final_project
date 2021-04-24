df9713886c3738ee5e96d67d202a4909ea64f64f
AR413 <s193254@student.dtu.dk>
2021-04-24 08:54:40 +0000

    update clean

--- R/01_load.R
+++ R/01_load.R
@@ -11,6 +11,7 @@ library("readxl")
 my_data_raw <- read_xls("data/_raw/pone.0207943.s001.xls")
 #my_data_raw
 
+
 # Write data
 write_tsv(x = my_data_raw,
           file = "data/01_my_data.tsv")
--- R/02_clean.R
+++ R/02_clean.R
@@ -4,13 +4,33 @@ rm(list = ls())
 
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
+library("magrittr")
 
 # Load data ---------------------------------------------------------------
-my_data <- read_tsv(file = "data/01_my_data.tsv")
+#Problem: the dataset has multiple headers
+#Solution: leave second explanatory header out of dataset but keep as dictionary
 
+#load only header
+my_data_header <- read_tsv(file = "data/01_my_data.tsv", n_max = 0) %>%
+  names()
+#my_data_header
+
+#load dataset except for the two first rows (headers) and then insert first row as header
+my_data <- read_tsv(file = "data/01_my_data.tsv", skip = 2, col_names = my_data_header)
+#my_data
+
+#save headers as key value pairs in dictionary
+headers_dict <- read_tsv(file = "data/01_my_data.tsv", n_max = 1) %>%
+  gather(variable_name, variable_description)
+#headers_dict
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean <- my_data # %>% ...
+#dim(my_data)
+my_data_clean <- distinct(my_data) %>% 
+  drop_na() 
+dim(my_data_clean)
+
+
 
 
 # Write data --------------------------------------------------------------
