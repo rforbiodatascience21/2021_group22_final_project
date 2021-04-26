9afcd3ecc3652a5710c6c57e4252400f71b66916
signeravn <s175410@student.dtu.dk>
2021-04-26 15:39:37 +0000

    code notes

--- R/05_modelling.R
+++ R/05_modelling.R
@@ -1,3 +1,6 @@
+library("tidyverse")
+library("broom")
+
 # Subset a number of proteins. Random ?
 set.seed(934485)
 corona_data_long_nested = corona_data_long_nested %>%
@@ -6,9 +9,17 @@ corona_data_long_nested = corona_data_long_nested %>%
 # Plot control vs corona-infected? Average over 3 replicates?
 # 4 different times - 2 hours, 6 hours, 10 hours, 24 hours
 
+## PCA (One script, 05_model_i.R --> Output 1 plot)
+# Color by control/infected (binomial, logaritmic)
+# Time and gene reduction
 
-# K means clustering (One script, 05_model_i.R --> Output 1 plot)
+#PCA analysis
+my_pca <- data %>% prcomp(center = TRUE, scale. = TRUE)
 
-# PCA (One script, 05_model_ii.R --> Output 1 plot)
+#tidy (using broom)
+my_pca %>% tidy()
 
-# Manhattan plot (One script, 05_model_iii.R --> Output 1 plot)
+#augment (broom)
+my_pca %>% augment()
+
+## K means clustering (One script, 05_model_ii.R --> Output 1 plot)
