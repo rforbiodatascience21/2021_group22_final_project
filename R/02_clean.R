ce45bfe01e6808dc2cbf908437ceaa8c572b7fe8
AR413 <s193254@student.dtu.dk>
2021-04-26 17:03:25 +0000

    Join datasets

--- R/02_clean.R
+++ R/02_clean.R
@@ -9,21 +9,24 @@ library("magrittr")
 # Load data ---------------------------------------------------------------
 my_data_counts <- read_tsv(file = "data/01_my_data_counts.tsv")
 my_data_samples <- read_tsv(file = "data/01_my_data_samples.tsv")
-my_data_counts
+#my_data_counts
+my_data_samples
 
 # Wrangle data ------------------------------------------------------------
 
-
-my_data_counts_wide <- rename(my_data_counts, genes = X1)
-my_data_counts_wide
-
-my_data_counts_wide <- my_data_counts_wide %>%
+#Counts
+my_data_counts_wide <- rename(my_data_counts, genes = X1) %>%
   pivot_longer(!genes, names_to = "experiment", values_to = "expression") %>%
   pivot_wider(names_from = "genes", values_from = "expression")
-my_data_counts_wide
+dim(my_data_counts_wide)
 
+#Samples
+my_data_samples <- rename(my_data_samples, experiment = X1) %>%
+  select(experiment, treatment, time, replicate)
+my_data_samples
 
-
+my_data_clean <- inner_join(my_data_counts_wide, my_data_samples, by="experiment")
+dim(my_data_clean)
 
 # Write data --------------------------------------------------------------
 write_tsv(x = my_data_clean,
