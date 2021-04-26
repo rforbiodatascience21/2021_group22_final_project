99ad4cdaccfcf87be2c5e802f8182ba452f03b26
signeravn <s175410@student.dtu.dk>
2021-04-26 14:59:35 +0000

    add notes

--- R/05_modelling.R
+++ R/05_modelling.R
@@ -1,5 +1,14 @@
-# K means clustering
+# Subset a number of proteins. Random ?
+set.seed(934485)
+corona_data_long_nested = corona_data_long_nested %>%
+  sample_n(100)
 
-#PCA
+# Plot control vs corona-infected? Average over 3 replicates?
+# 4 different times - 2 hours, 6 hours, 10 hours, 24 hours
 
-# Manhattan plot (?)
+
+# K means clustering (One script, 05_model_i.R --> Output 1 plot)
+
+# PCA (One script, 05_model_ii.R --> Output 1 plot)
+
+# Manhattan plot (One script, 05_model_iii.R --> Output 1 plot)
