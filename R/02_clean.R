f2d497e697e83de42ea75567108d7c4c1b4bffe9
Jens Waaben <waabenjens@gmail.com>
2021-04-30 12:02:10 +0000

    Fixed strings in clean to iclude _1 for first sample and deleted whitespaces - for some reason it would not do it in first try - someone can look it through?

--- R/02_clean.R
+++ R/02_clean.R
@@ -8,23 +8,32 @@ library("tidyverse")
 # Load data ---------------------------------------------------------------
 my_data_counts <- read_tsv(file = "data/01_my_data_counts.tsv")
 my_data_samples <- read_tsv(file = "data/01_my_data_samples.tsv")
-#my_data_counts
+
+
 my_data_samples
 
 # Wrangle data ------------------------------------------------------------
 
 #Counts
-my_data_counts_wide <- rename(my_data_counts, genes = X1) %>%
-  pivot_longer(!genes, names_to = "experiment", values_to = "expression") %>%
+my_data_counts_wide <- my_data_counts %>% 
+  rename(genes = X1) %>%
+  pivot_longer(-genes, names_to = "experiment", values_to = "expression") %>% 
   pivot_wider(names_from = "genes", values_from = "expression")
-dim(my_data_counts_wide)
+
 
 #Samples
 my_data_samples <- rename(my_data_samples, experiment = X1) %>%
   select(experiment, treatment, time, replicate)
 my_data_samples
 
-my_data_clean <- inner_join(my_data_counts_wide, my_data_samples, by="experiment")
+my_data_clean <- inner_join(my_data_counts_wide, my_data_samples, by="experiment") %>% 
+  mutate(experiment = str_replace_all(experiment,
+                                      c("\\dh$", "[ ]"),
+                                      c("\\dh$" = "2h_1",
+                                        "[ ]" = "_")), # didn't work to just do it once - anybody has an idea why?
+         experiment = str_replace_all(experiment,
+                                      c("[ ]"),
+                                      c("[ ]" = "_")))
 dim(my_data_clean)
 
 # Write data --------------------------------------------------------------
--- R/04_exploratory_data_analysis.R
+++ R/04_exploratory_data_analysis.R
@@ -2,5 +2,6 @@
 
 library("tidyverse")
 
-data = read_tsv("data/03_dat_aug.tsv")
+data = read_tsv("data/02_my_data_clean.tsv")
 
+data2 = read_tsv("data/01_my_data_counts.tsv")
