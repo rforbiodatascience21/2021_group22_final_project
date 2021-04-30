aad78f61cea8e83f2a3795daaaad7ca6358d86da
Jens Waaben <waabenjens@gmail.com>
2021-04-30 09:03:14 +0000

    dele placeholder in doc

--- R/03_data_augment_jens.R
+++ R/03_data_augment_jens.R
@@ -3,6 +3,7 @@
 library("tidyverse")
 library("purrr")
 
-data <- read_tsv("data/02_my_data_clean.tsv") %>% 
-  mutate(treatment = case_when(experiment == ))
-  
+data <- read_tsv("data/02_my_data_clean.tsv") 
+
+ggplot(data = data, mapping = aes(x = AAAS, y = AACS))
+
