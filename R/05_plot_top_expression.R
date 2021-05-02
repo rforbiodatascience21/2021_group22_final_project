1621d177f96ae70957a62e47f411ec23b718eff4
albanobel <albanlos@gmail.com>
2021-05-02 12:22:40 +0000

    05 save plot

--- R/05_plot_top_expression.R
+++ R/05_plot_top_expression.R
@@ -10,9 +10,9 @@ num_genes <- 20
 data_sorted_long <- data_sorted %>% select(1:all_of(num_genes+2)) %>%
   pivot_longer(!c(treatment, time), names_to = "gene", values_to = "count")
 
-order_names <- data_sorted_long %>% ungroup() %>% slice_head(n=20) %>% pull(gene) %>% factor()
+order_names <- data_sorted_long %>% ungroup() %>% slice_head(n=num_genes) %>% pull(gene) %>% factor()
 
-ggplot(data = data_sorted_long, aes(factor(gene), count, color = time, shape=treatment)) +
+ggplot(data = data_sorted_long, aes(factor(gene, level = order_names), count, color = time, shape=treatment)) +
   geom_point() +
   theme(axis.text.x = element_text(angle = -45, vjust = -0.6, hjust=0.4)) +
   scale_y_log10()
