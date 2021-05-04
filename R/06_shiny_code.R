f0a07e41992c8bbcf30dfaa0d0d3707e18d19ca9
Jens Waaben <waabenjens@gmail.com>
2021-05-04 20:25:47 +0000

    Shiny app tried the plot for showing the gene over time

--- R/06_shiny_code.R
+++ R/06_shiny_code.R
@@ -3,7 +3,7 @@ library("shiny")
 library("shinythemes")
 
 #Load data
-data_for_plot_raw <- read_tsv("../data/03_data_mean_log2.tsv")
+data_for_plot_raw <- read_tsv("data/03_data_mean_log2.tsv")
 
 data_for_plot_raw
 
@@ -13,31 +13,34 @@ data_for_plot <- data_for_plot_raw %>%
                names_to = "genes",
                values_to = "log_fold_change") 
 
+# Loading data for one of the subpanels: 
+data_times_seperated <- read_tsv("results/05_individual_times_ttest_and_data.tsv")
 
+# making list of genes to choose between: 
+unique_gene_names <- data_times_seperated %>% 
+  select(genes) %>% 
+  distinct() %>% 
+  arrange(genes)
 
 # Define UI
-ui <- fluidPage(theme = shinytheme("yeti"),
-                titlePanel("Gene expression x hours after infection with coronavirus compared to control"),
-                
-                sidebarPanel(
-                  selectInput("genes", "Gene:",
-                              choices = c("genes"))
-                ),
-                mainPanel('Boxplot', plotOutput("myplot"))
-                
-                
-
-) # fluidPage
+ui <- fluidPage(theme = shinytheme("cerulean"),
+                navbarPage("My first navbar page",
+                           tabPanel("navbar 1", 
+                                    sidebarPanel(selectInput(inputId = "bum",
+                                                             "Gene:",
+                                                             choices = unique_gene_names)),
+                                    mainPanel('Boxplot', plotOutput("myplot"))
+                                    ),
+                           tabPanel("navbar 2"),
+                           tabPanel("navbar 3")
+                            )
+                ) # fluidPage
 
 
 # Define server function  
 server <- function(input, output) {
-  
   output$myplot <- renderPlot({
-    
-    
     boxplot((data$genes), data = "data")
-    
   })
 } # server
 
