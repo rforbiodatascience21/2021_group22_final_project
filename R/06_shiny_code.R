c9dd8d3f815b98036029cefad40e8288de9e1614
Jens Waaben <waabenjens@gmail.com>
2021-05-04 21:30:34 +0000

    Shiny app now kinda works...

--- R/06_shiny_code.R
+++ R/06_shiny_code.R
@@ -25,11 +25,12 @@ unique_gene_names <- data_times_seperated %>%
 # Define UI
 ui <- fluidPage(theme = shinytheme("cerulean"),
                 navbarPage("My first navbar page",
-                           tabPanel("navbar 1", 
-                                    sidebarPanel(selectInput(inputId = "bum",
+                           tabPanel("Protein expression per time", 
+                                    sidebarPanel(selectInput(inputId = "Selected_gene",
                                                              "Gene:",
-                                                             choices = unique_gene_names)),
-                                    mainPanel('Boxplot', plotOutput("myplot"))
+                                                             choices = unique_gene_names,
+                                                             selected = "	44085")),
+                                    mainPanel(plotOutput("myplot"))
                                     ),
                            tabPanel("navbar 2"),
                            tabPanel("navbar 3")
@@ -39,10 +40,17 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
 
 # Define server function  
 server <- function(input, output) {
+  temp_tibble_for_plotting <- reactive(data_times_seperated  %>% 
+    filter(genes == input$Selected_gene))
+  
   output$myplot <- renderPlot({
-    boxplot((data$genes), data = "data")
-  })
-} # server
+    ggplot(temp_tibble_for_plotting(),
+           mapping = aes(x = time,
+                         y = normalized_counts,
+                         fill = treatment)) +
+    geom_boxplot()
+    })
+}  #server
 
 
 # Create Shiny object
