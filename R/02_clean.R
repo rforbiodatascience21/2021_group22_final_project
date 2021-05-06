7710774c503d9d9ed781981119686eeae10dfec0
AR413 <s193254@student.dtu.dk>
2021-05-06 10:34:33 +0000

    replace zero values

--- R/02_clean.R
+++ R/02_clean.R
@@ -11,10 +11,11 @@ my_data_samples <- read_tsv(file = "data/01_my_data_samples.tsv")
 
 # Wrangle data ------------------------------------------------------------
 
-#Counts
+#Prepare counts dataset for joining and replace zero values with low value 
 my_data_counts_wide <- my_data_counts %>% 
   rename(genes = X1) %>%
-  pivot_longer(-genes, names_to = "experiment", values_to = "expression") %>% 
+  pivot_longer(-genes, names_to = "experiment", values_to = "expression") %>%
+  mutate(expression = replace(expression, 0, 0.0000001)) %>%
   pivot_wider(names_from = "genes", values_from = "expression")
 
 
@@ -28,6 +29,7 @@ my_data_samples <- my_data_samples %>%
 my_data_clean <- inner_join(my_data_samples, my_data_counts_wide, by="experiment") %>% 
   mutate(experiment = str_replace_all(experiment, c("\\dh$" = "2h_1",
                                                     "[ ]" = "_")))
+  
 
 # Write data --------------------------------------------------------------
 write_tsv(x = my_data_clean,
