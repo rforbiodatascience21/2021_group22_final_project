f78422b4cebbea98bf973341f7c99e50c39fd6ba
AR413 <s193254@student.dtu.dk>
2021-05-07 15:35:53 +0000

    Heatmap update

--- R/04_heatmap.R
+++ R/04_heatmap.R
@@ -6,12 +6,42 @@ rm(list = ls())
 library("tidyverse")
 
 # Load data ---------------------------------------------------------------
-data_normalized = read_tsv("data/03_normalized_counts_and_raw_counts.tsv")
+clean_data = read_tsv("data/02_my_data_clean.tsv")
 data_sorted <- read_tsv(file = "data/03_data_mean_log2.tsv")
-# Wrangle data ------------------------------------------------------------
-head(data_normalized)
 
-num_genes <- 50
+# Wrangle data ------------------------------------------------------------
+head(clean_data)
+
+long_data <- clean_data %>%
+  pivot_longer(cols = c(-experiment, -treatment, -time, -replicate), 
+               names_to = "genes", values_to = "counts") %>%
+  select(treatment, time, genes, counts) %>%
+  unite("experiment", treatment, time, sep = "_", remove = TRUE) %>%
+  group_by(experiment, genes) %>%
+  mutate(mean_counts = sum(counts)/3) %>%
+  select(experiment, genes, mean_counts) %>%
+  distinct()
+
+
+head(long_data)
+dim(long_data)
+
+calculate_zscore <- long_data %>%
+  group_by(genes) %>%
+  mutate(mean_counts_for_gene = mean(mean_counts)) %>%
+  mutate(sd_of_counts_for_gene = sd(mean_counts)) %>%
+  mutate(z_score_step1 = mean_counts-mean_counts_for_gene) %>%
+  mutate(z_score = z_score_step1/sd_of_counts_for_gene) %>%
+  ungroup() %>%
+  arrange(mean_counts_for_gene)
+
+head(calculate_zscore, n = 10)
+dim(calculate_zscore)
+
+
+# Wrangle data ------------------------------------------------------------
+
+num_genes <- 500
 
 # Select the top n differentially expressed genes for plotting
 # Then pivot longer to get 1 gene per row
@@ -20,30 +50,40 @@ data_sorted_long <- data_sorted %>%
   pivot_longer(!c(treatment, time),
                names_to = "genes",
                values_to = "count") %>%
-  distinct(genes)  
-dim(data_sorted_long)
+  distinct(genes)
+
 head(data_sorted_long)
 
-# 
-data_plot <- data_normalized %>%
-  select(experiment, genes, normalized_counts) %>%
-  group_by(genes) %>%
-  mutate(z_score = normalized_counts-mean(normalized_counts)/sd(normalized_counts)) %>%
-  ungroup()
-head(data_plot)
+data_plot3 <- semi_join(calculate_zscore, data_sorted_long, by="genes") %>%
+  select(experiment) %>%
+  rename(C1 = Control_2h)
 
-data_plot2 <- data_plot %>%
-  select(experiment, genes, z_score)
-
-data_plot3 <- right_join(data_plot2, data_sorted_long, by="genes")
-
-head(data_plot3)
+  
+  
 dim(data_plot3)
-head(data_plot3)
+head(data_plot3, n = 10)
 
-ggplot(data = data_plot3, aes(x = experiment, genes)) +
+
+ggplot(data = data_plot3, aes(x = rowid[1:8], y = genes)) +
   geom_tile(aes(fill = z_score)) +
   scale_fill_gradient2(low = "yellow", high = "red")
+  #scale_x_discrete(labels=c("Control 2h", "Control 6h", "Control ","1h","2h","3h","6h","12h","24h","48h"))
 
 ggsave(path = "results",
        filename = "Heatmap.png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
