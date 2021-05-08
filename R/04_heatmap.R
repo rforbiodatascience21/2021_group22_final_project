c8b2f38a7398fb8928eded7942c68bb3fe30c884
AR413 <s193254@student.dtu.dk>
2021-05-08 16:14:15 +0000

    clean up script

--- R/04_heatmap.R
+++ R/04_heatmap.R
@@ -10,11 +10,11 @@ clean_data = read_tsv("data/02_my_data_clean.tsv")
 data_sorted <- read_tsv(file = "data/03_data_mean_log2.tsv")
 
 # Wrangle data ------------------------------------------------------------
-head(clean_data)
-
+# Find mean of replicates
 long_data <- clean_data %>%
   pivot_longer(cols = c(-experiment, -treatment, -time, -replicate), 
-               names_to = "genes", values_to = "counts") %>%
+               names_to = "genes", 
+               values_to = "counts") %>%
   select(treatment, time, genes, counts) %>%
   unite("experiment", treatment, time, sep = "_", remove = TRUE) %>%
   group_by(experiment, genes) %>%
@@ -22,29 +22,19 @@ long_data <- clean_data %>%
   select(experiment, genes, mean_counts) %>%
   distinct()
 
-
-head(long_data)
-dim(long_data)
-
-calculate_zscore <- long_data %>%
+# Calculate z-score
+data_zscore <- long_data %>%
   group_by(genes) %>%
-  mutate(mean_counts_for_gene = mean(mean_counts)) %>%
-  mutate(sd_of_counts_for_gene = sd(mean_counts)) %>%
-  mutate(z_score_step1 = mean_counts-mean_counts_for_gene) %>%
-  mutate(z_score = z_score_step1/sd_of_counts_for_gene) %>%
+  mutate(mean_counts_for_gene = mean(mean_counts),
+         sd_of_counts_for_gene = sd(mean_counts),
+         count_minus_mean = mean_counts-mean_counts_for_gene,
+         z_score = count_minus_mean/sd_of_counts_for_gene) %>%
   ungroup() %>%
-  arrange(mean_counts_for_gene)
+  select(experiment, genes, z_score)
 
-head(calculate_zscore, n = 10)
-dim(calculate_zscore)
-
-
-# Wrangle data ------------------------------------------------------------
-
+# Find the top n deferentially expressed genes to plot
 num_genes <- 500
 
-# Select the top n differentially expressed genes for plotting
-# Then pivot longer to get 1 gene per row
 data_sorted_long <- data_sorted %>%
   select(1:all_of(num_genes+2)) %>%
   pivot_longer(!c(treatment, time),
@@ -52,32 +42,21 @@ data_sorted_long <- data_sorted %>%
                values_to = "count") %>%
   distinct(genes)
 
-head(data_sorted_long)
+# Only keep z-score for the top n deferentially expressed genes chosen earlier
+data_plot <- semi_join(data_zscore, data_sorted_long, by="genes")
 
-data_plot3 <- semi_join(calculate_zscore, data_sorted_long, by="genes")
+# Plot and save heatmap
+data_plot %>%
+  mutate(experiment = as_factor(experiment)) %>%
+  ggplot(aes(y=genes, x=experiment, fill=z_score)) + 
+  geom_tile() +
+  scale_fill_gradient2(low = "yellow", high = "red") + 
+  theme(axis.ticks.y = element_blank(),
+        axis.text.y = element_blank())
 
-  
-  
-dim(data_plot3)
-head(data_plot3, n = 10)
-
-
-ggplot(data = data_plot3, aes(x = experiment, y = genes)) +
-  geom_tile(aes(fill = z_score)) +
-  scale_fill_gradient2(low = "yellow", high = "red")
-  #scale_x_discrete(labels=c("Control 2h", "Control 6h", "Control ","1h","2h","3h","6h","12h","24h","48h"))
 
 ggsave(path = "results",
        filename = "Heatmap.png")
-
-
-
-
-
-
-
-
-
 
 
 
