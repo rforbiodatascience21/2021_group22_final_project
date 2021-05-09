30b9e7d28b348d48be17569ce5ddf0f6a10e0226
signeravn <s175410@student.dtu.dk>
2021-05-09 11:11:35 +0000

    update augment - new version of sort

--- R/03_data_augment.R
+++ R/03_data_augment.R
@@ -57,24 +57,41 @@ data_mean_log2_diff <- data_mean_log2 %>%
   group_by(time) %>%
   summarise_if(is.numeric, diff)          #virus minus control eller omvendt?
 
-# Signes bud på log2
+## Signes bud på log2 difference -----------
 # Calculate log2 diff (Maybe change names)
-new_data_mean_log2_diff <- data_mean_log2 %>%
+new_data_mean_log2_diff_long <- data_mean_log2 %>%
   pivot_longer(cols = c(-time, -treatment), names_to = "genes", values_to = "log2") %>%
   pivot_wider(names_from = "treatment", values_from = "log2") %>%
   group_by(time) %>% 
   mutate(log2_diff = Virus-Control) %>%
-  ungroup()
+  ungroup() %>%
+  select(-Virus, -Control)
 
 # Convert back to tidy data (maybe change names)
-new_data_mean_log2_diff <- new_data_mean_log2_diff %>%
-  select(-Virus, -Control) %>%
+new_data_mean_log2_diff <- new_data_mean_log2_diff_long %>%
   pivot_wider(names_from = "genes", values_from = "log2_diff")
 
+## Signes bud sort ---------------
+sorted_genes <- new_data_mean_log2_diff_long %>%
+  arrange(desc(log2_diff)) %>%
+  select(genes, time)
 
+# Change the dataframe with means to fit the long format
+data_mean_long <- data_mean %>% 
+  pivot_longer(cols = c(-time, -treatment), names_to = "genes", values_to = "counts") 
+
+# Change to order of the data mean long after highest logfold expression
+sorted_means <- sorted_genes %>%
+  full_join(x = ., y = data_mean_long, by = c("genes", "time"))
+
+# Convert the means back to tidy data format
+sorted_means_wide <- sorted_means %>%
+  pivot_wider(names_from = "genes", values_from = "counts")
+
+## original sort -----------
 # Extract order of highest differential expression based on 2, 6, 10 or 24 hours
 # Put time in rows
-data_mean_log2_diff_2 <- data_mean_log2_diff %>% 
+data_mean_log2_diff_2 <- new_data_mean_log2_diff %>% 
   column_to_rownames(var = "time")
 
 rownum <- 4   # 4 = 24h
@@ -103,6 +120,5 @@ write_tsv(x = data_sorted,
 
 write_tsv(x = new_data_mean_log2_diff,
           file = "data/03_data_mean_log2_diff.tsv")
-
 
 
